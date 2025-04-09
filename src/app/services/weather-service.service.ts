7398ff7f67f2d402f45af3e3c06dd07bbf6002ab
hitesh4056 <hitesh@ostaapp.com>
2025-04-08 23:53:09 -0400

    changes

--- src/app/services/weather-service.service.ts
+++ src/app/services/weather-service.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root' // This makes sure the service is available app-wide
 })
 export class WeatherService {
-  private apiUrl = 'http://api.weatherstack.com/current?access_key=cab12f1f973ae6c2abdd8a38541ab668';
+  private apiUrl = 'https://api.weatherstack.com/current?access_key=cab12f1f973ae6c2abdd8a38541ab668';
 
   constructor(private http: HttpClient) {}
 
